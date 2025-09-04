94908462f0dd974c9db9dce2926e1ce30072e50c
aditya250400 <muhamadrizkiaditya32@gmail.com>
2025-09-04 17:55:49 +0700

    fix dashboard

--- routes/admin.php
+++ routes/admin.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth'])->group(function () {
 
     //dashboard
-    Route::get('dashboard', DashboardAdminController::class)->name('admin.dashboard');
+    Route::get('/dashboard', DashboardAdminController::class)->name('admin.dashboard');
 
     // levels
     Route::controller(LevelController::class)->group(function () {
